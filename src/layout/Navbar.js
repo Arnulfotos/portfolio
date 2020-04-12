6c9eea6f215209c7a05d7f6a3176a81cfd25911f
Abhi Velaga <abhinav.velaga@gmail.com>
2020-04-12 00:58:20 -0500

    added link color animation to all links

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -5,50 +5,56 @@ export class Navbar extends Component {
   render() {
     return (
       <div className="flex navbar">
-
         <a href="/photo">
           <div className="link">
-
             <div className="link-color"></div>
-
             <div className="link-block">
               <div className="link-piece"></div>
               <div className="link-piece"></div>
               <div className="link-piece"></div>
               <div className="link-piece">PHOTO</div>
             </div>
-
           </div>
         </a>
 
-        <div className="link animation">
-          <div className="link-piece"></div>
-          <div className="link-piece"></div>
-          <div className="link-piece"></div>
-          <div className="link-piece">MUSIC</div>
-        </div>
-        <div className="link animation">
-          <div className="link-piece"></div>
-          <div className="link-piece"></div>
-          <div className="link-piece"></div>
-          <div className="link-piece">CODE</div>
-        </div>
-        <div className="link animation">
-          <div className="link-piece"></div>
-          <div className="link-piece"></div>
-          <div className="link-piece"></div>
-          <div className="link-piece">ABOUT</div>
-        </div>
+        <a href="/music">
+          <div className="link">
+            <div className="link-color"></div>
+            <div className="link-block">
+              <div className="link-piece"></div>
+              <div className="link-piece"></div>
+              <div className="link-piece"></div>
+              <div className="link-piece">MUSIC</div>
+            </div>
+          </div>
+        </a>
+
+        <a href="/code">
+          <div className="link">
+            <div className="link-color"></div>
+            <div className="link-block">
+              <div className="link-piece"></div>
+              <div className="link-piece"></div>
+              <div className="link-piece"></div>
+              <div className="link-piece">CODE</div>
+            </div>
+          </div>
+        </a>
+
+        <a href="/about">
+          <div className="link">
+            <div className="link-color"></div>
+            <div className="link-block">
+              <div className="link-piece"></div>
+              <div className="link-piece"></div>
+              <div className="link-piece"></div>
+              <div className="link-piece">ABOUT</div>
+            </div>
+          </div>
+        </a>
       </div>
     )
   }
 }
 
 export default Navbar
-
-// const navbar = {
-//   width: '50vw',
-//   height: '8vh',
-//   backgroundColor: 'white',
-//   float: 'right',
-// }
