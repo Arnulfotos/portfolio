db26fc2ae91659faab4b29cd894572cf10eeeed6
Abhi Velaga <abhinav.velaga@gmail.com>
2020-05-07 01:04:01 -0500

    music grid with main and cover imgs working

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -1,6 +1,20 @@
 import React, { Component } from "react";
 import Navbar from '../layout/Navbar'
+import indulgent from '../../dist/images/music/indulgent/drums.jpg';
+import indulgentCover from '../../dist/images/music/indulgent/cover.jpg';
+
+import mjf from '../../dist/images/music/mjf/mjf.jpg';
+import mjfCover from '../../dist/images/music/mjf/cover.jpg';
+
+import cg from '../../dist/images/music/cg/cg.jpg';
+import cgCover from '../../dist/images/music/cg/cover.jpg';
 import './pages.css';
+
+const image = {
+  width: '100vw'
+}
+
+
 
 export class Music extends Component {
   render() {
@@ -8,7 +22,28 @@ export class Music extends Component {
       <Navbar activeLink={"MUSIC"} />
       <div className="page">
         <div className="appear">
-          <h1>THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE THIS IS THE MUSIC PAGE </h1>
+          <div className="grid">
+            <div className="indulgent">
+              {/* <img src={indulgent} style={image} /> */}
+            </div>
+            <div className="indulgent-cover">
+              {/* <img src={indulgentCover} style={{ width: '100%' }} /> */}
+            </div>
+            
+            <div className="mjf">
+              {/* <img src={mjf} style={image} /> */}
+            </div>
+            <div className="mjf-cover">
+              {/* <img src={mjfCover} style={{ width: '100%' }} /> */}
+            </div>
+
+            <div className="cg">
+              {/* <img src={cg} style={image} /> */}
+            </div>
+            <div className="cg-cover">
+              {/* <img src={cgCover} style={{ width: '100%' }} /> */}
+            </div>
+          </div>
         </div>
       </div>
     </div>;
