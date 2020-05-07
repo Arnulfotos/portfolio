97fd16478f24d8639452fc248a9fe4070caf4a19
Abhi Velaga <abhinav.velaga@gmail.com>
2020-05-07 02:42:47 -0500

    added music titles

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -5,8 +5,24 @@ import './pages.css';
 import guitar1 from '../../dist/images/music/indulgent/guitar1.jpg';
 import guitar2 from '../../dist/images/music/indulgent/guitar2.jpg';
 
-const image = {
-  width: '33vw'
+const title = {
+  color: 'white',
+  fontSize: '200px',
+  textAlign: 'center'
+}
+
+const mjfTitle = {
+  color: 'black',
+  fontSize: '100px',
+  textAlign: 'center',
+  lineHeight: '90px'
+}
+
+const cgTitle = {
+  color: 'white',
+  fontSize: '150px',
+  textAlign: 'right',
+  lineHeight: '130px'
 }
 
 
@@ -18,16 +34,22 @@ export class Music extends Component {
       <div className="page">
         <div className="appear">
           <div className="grid">
-            <div className="indulgent"></div>
+            <div className="indulgent">
+              <div style={title}>INDULGENT</div>
+            </div>
             <div className="indulgent-cover"></div>
             <div className="indulgent-guitar1" />
             <div className="indulgent-guitar2" />
 
-            <div className="mjf"></div>
+            <div className="mjf">
+            <div style={mjfTitle}>MERRY JANE & THE FONDAS</div>
+            </div>
             <div className="mjf-studio"></div>
             <div className="mjf-cover"></div>
 
-            <div className="cg"></div>
+            <div className="cg">
+              <div style={cgTitle}>CREAM GENIE</div>
+            </div>
             <div className="cg-cover"></div>
             <div className="cg-guitar"></div>
           </div>
