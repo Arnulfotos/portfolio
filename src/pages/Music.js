b29ad1dda636b1a49b6feeb2102681aa385c4ff2
Abhi Velaga <abhinav.velaga@gmail.com>
2020-05-07 01:49:12 -0500

    finished music background

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -1,17 +1,12 @@
 import React, { Component } from "react";
 import Navbar from '../layout/Navbar'
-import indulgent from '../../dist/images/music/indulgent/drums.jpg';
-import indulgentCover from '../../dist/images/music/indulgent/cover.jpg';
-
-import mjf from '../../dist/images/music/mjf/mjf.jpg';
-import mjfCover from '../../dist/images/music/mjf/cover.jpg';
-
-import cg from '../../dist/images/music/cg/cg.jpg';
-import cgCover from '../../dist/images/music/cg/cover.jpg';
 import './pages.css';
 
+import guitar1 from '../../dist/images/music/indulgent/guitar1.jpg';
+import guitar2 from '../../dist/images/music/indulgent/guitar2.jpg';
+
 const image = {
-  width: '100vw'
+  width: '33vw'
 }
 
 
@@ -23,26 +18,18 @@ export class Music extends Component {
       <div className="page">
         <div className="appear">
           <div className="grid">
-            <div className="indulgent">
-              {/* <img src={indulgent} style={image} /> */}
-            </div>
-            <div className="indulgent-cover">
-              {/* <img src={indulgentCover} style={{ width: '100%' }} /> */}
-            </div>
-            
-            <div className="mjf">
-              {/* <img src={mjf} style={image} /> */}
-            </div>
-            <div className="mjf-cover">
-              {/* <img src={mjfCover} style={{ width: '100%' }} /> */}
-            </div>
+            <div className="indulgent"></div>
+            <div className="indulgent-cover"></div>
+            <div className="indulgent-guitar1" />
+            <div className="indulgent-guitar2" />
 
-            <div className="cg">
-              {/* <img src={cg} style={image} /> */}
-            </div>
-            <div className="cg-cover">
-              {/* <img src={cgCover} style={{ width: '100%' }} /> */}
-            </div>
+            <div className="mjf"></div>
+            <div className="mjf-studio"></div>
+            <div className="mjf-cover"></div>
+
+            <div className="cg"></div>
+            <div className="cg-cover"></div>
+            <div className="cg-guitar"></div>
           </div>
         </div>
       </div>
