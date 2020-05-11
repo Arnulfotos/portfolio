469261099fc53368628ce2b7a1cb7f2f7070c314
Abhi Velaga <abhinav.velaga@gmail.com>
2020-05-10 22:07:42 -0500

    changed pi url to public url

--- src/pages/Hi.js
+++ src/pages/Hi.js
@@ -11,7 +11,7 @@ export class Hi extends Component {
   }
 
   onClick = () => {
-    let url = "http://192.168.1.170:8000/led";
+    let url = "http://72.182.112.17:8000/led";
     axios
       .get(
         url
