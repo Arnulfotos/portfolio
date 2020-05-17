cdc8c9822210618c270e5a290d5c61ae8d373947
Abhi Velaga <abhinav.velaga@gmail.com>
2020-05-16 23:33:08 -0500

    basic software desktop done

--- src/pages/Software.js
+++ src/pages/Software.js
@@ -6,14 +6,11 @@ import Navbar from '../layout/Navbar'
 import Columned from "react-columned";
 import './pages.css';
 
-import color1 from '../../dist/images/software/color1.jpg';
-import color2 from '../../dist/images/software/color2.jpg';
-import color3 from '../../dist/images/software/color3.jpg';
-import color4 from '../../dist/images/software/color4.jpg';
-import color5 from '../../dist/images/software/color5.jpg';
-import color6 from '../../dist/images/software/color5.jpg';
-import color7 from '../../dist/images/software/color5.jpg';
-import color8 from '../../dist/images/software/color5.jpg';
+import color1 from '../../dist/images/software/color1.png';
+import color2 from '../../dist/images/software/color2.png';
+import color3 from '../../dist/images/software/color3.png';
+import color4 from '../../dist/images/software/color4.png';
+import color5 from '../../dist/images/software/color5.png';
 import ball from '../../dist/images/software/ball.gif';
 import color_animated from '../../dist/images/software/color_animated.gif';
 import dot_terrain from '../../dist/images/software/dot_terrain.gif';
@@ -24,6 +21,10 @@ import sin from '../../dist/images/software/sin.gif';
 import slb from '../../dist/images/software/slb.jpg';
 import bandTogether from '../../dist/images/software/bandtogether.png';
 import bandTogether1 from '../../dist/images/software/bandtogether1.png';
+import leaseEasy from '../../dist/images/software/leaseasy.png';
+import stairs from '../../dist/images/software/stairs.gif';
+import bag from '../../dist/images/software/bag.gif';
+import desk from '../../dist/images/software/desk.png';
 
 const images = [
   slb,
@@ -37,13 +38,10 @@ const images = [
   color_animated,
   color2,
   bandTogether,
-  bandTogether1
-  // color3,
-  // color4,
-  // color5,
-  // color6,
-  // color7,
-  // color8,
+  bandTogether1,
+  color3,
+  color4,
+  color5,
 ];
 
 export class Software extends Component {
@@ -65,13 +63,73 @@ export class Software extends Component {
 
           <MediaQuery minDeviceWidth={500}>
             <div className="software">
-              <div className="title software-title">SOFTWARE DEVELOPMENT</div>
-              <div className="software-text">some of my projects</div>
+              <div className="software-intro">
+                intro description talking about how i major in cs but also enjoying finding ways to intersect it with art. this is a colleciton of a range of some of my programming and web related projects. find my cs skills list at linkedin (link it)
+              </div>
+              <div className="software1 back-img">
+                <img src={bandTogether} className="software-ft" />
+                <div className="software-ft-title">BAND TOGETHER</div>
+                <div className="software-ft-subtitle">ui design + front end development</div>
+                <div className="software-ft-info">this is the description this is the description this is the description this is the description this is the description this is the description this is the description this is the description </div>
+                <div className="software-ft-button">VIEW</div>
+              </div>
+
+              <div className="software2">
+                <div className="flex">
+                  <img src={dot_terrain} className="software2-img" />
+                  <img src={sin} className="software2-img" />
+                  <img src={evolution} className="software2-img" />
+                </div>
+                <div className="software-ft-title">COMPUTER GRAPHICS</div>
+                <div className="software-ft-info">i enjoy exploring more creative uses for computer science in my free time. this is some of my graphics work created with processing</div>
+                <div className="software-ft-button">SEE MORE</div>
+              </div>
+
+              <div className="software3 back-img">
+                <img src={leaseEasy} className="software-ft" />
+                <div className="software-ft-title">LEASE EASY</div>
+                <div className="software-ft-subtitle">ui design</div>
+                <div className="software-ft-info">this is the description this is the description this is the description this is the description this is the description this is the description this is the description this is the description </div>
+              </div>
+
+              < img src={led_zoo} style={fullWidth} />
+              <div className="software-grid">
+                <div className="software-grid-text">
+                  <div className="grid-text-title">LED STRIP PROGRAMMING</div>
+                this is the descirp tion this is the descirp tion this is the descirp tion this is the descirp tion this is the descirp tion this is the descirp tion
+                </div>
+                <div className="grid-stairs">
+                  <img src={stairs} style={fullWidth} />
+                </div>
+
+                <div className="grid-bag">
+                  <img src={bag} style={fullWidth} />
+                </div>
+
+                <div className="grid-slb">
+                  <img src={slb} style={fullWidth} />
+                </div>
+
+                <div className="grid-slb-text">
+                  talking about how i used it on my presentation talking about how i used it on my presentation talking about how i used it on my presentation talking about how i used it on my presentation talking about how i used it on my presentation
+                </div>
+              </div>
+
+              <div className="desk back-img flex">
+                <div>I'm available for freelance web design + development!</div>
+                <div className="software-ft-button">Get in touch!</div>
+              </div>
+
+              {/* <div className="software4 back-img"></div> */}
+
+              {/* <div className="title software-title">SOFTWARE DEVELOPMENT</div>
+              <div className="software-text">i'm a computer science major at ut austin with a passion for front end/ux/ui. i have experience with react and angular and love to make websites in my free time. this is some of the work ive developed.</div>
               <img src={slb} style={setHeight} />
               <img src={bandTogether} style={setHeight} />
               <img src={bandTogether1} style={setHeight} />
 
               <div className="title software-title">COMPUTER GRAPHICS</div>
+              <div className="software-text">i enjoy exploring more creative uses for computer science in my free time. this is some of my graphics work created with processing</div>
               <img src={dot_terrain} style={setHeight} />
               <img src={sin} style={setHeight} />
               <img src={color1} style={setHeight} />
@@ -79,16 +137,11 @@ export class Software extends Component {
               <img src={ball} style={setHeight} />
               <img src={color_animated} style={setHeight} />
               <img src={color2} style={setHeight} />
-              {/* <img src={miami} style={setHeight}/> */}
 
               <div className="title software-title">LEDs/ARDUINO/RASPBERRY PI</div>
-              <img src={led_zoo} style={setHeight} />
+              <div className="software-text">i also love using software alongside electronics, especially individually adressable LED strips, to interact with the physical world.</div>
+              <img src={led_zoo} style={setHeight} /> */}
 
-              {/* <Columned columns={3} className="gallery">
-              {images.map((value, index) => {
-                return <img className="img" key={index} src={value} onClick={() => this.setState({ isOpen: true, photoIndex: index })} />
-              })}
-            </Columned> */}
             </div>
 
           </MediaQuery>
@@ -127,7 +180,10 @@ export class Software extends Component {
 
 export default Software;
 
-const setHeight = {
-  height: '300px',
-  margin: '5px'
+const fullWidth = {
+  width: '100%'
+}
+
+const fullHeight = {
+  height: '100%'
 }
